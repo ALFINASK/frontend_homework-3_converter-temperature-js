b897dea559d417012428223d44884188e5f56fff
ALFINASK <alfina.sukma.tif17@polban.ac.id>
2021-08-24 00:26:51 +0700

    fix: js for testing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,3 @@
-// const { Converter } = require("./converter");
-
-class Converter {
-    constructor() {
-    }
-
-    celcius(to, number) {
-        if (to == "celcius") {
-            return number;
-        }
-
-        if (to == "farenheit") {
-            return (number * (9 / 5)) + 32;
-        }
-    }
-    
-    farenheit(to, number) {
-        if (to == "farenheit") {
-            return number;
-        }
-        if (to == "celcius") {
-            return (number - 32) * (5 / 9);
-        }
-    }
-}
-
 function emptyForm() {
     document.getElementById("temperature").value = "";
     document.getElementById("result").value = "";
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,18 @@
 const { Converter } = require("../src/scripts/converter");
 
 const converter = new Converter();
-test("celcius 8 + equal 10", () => {
+test("from 10 celcius to 50 farenheit", () => {
     expect(converter.celcius("farenheit", 10)).toBe(50);
+});
+
+test("from 10 celcius to 10 celcius", () => {
+    expect(converter.celcius("celcius", 10)).toBe(10);
+});
+
+test("from 50 farenheit to 50 farenheit", () => {
+    expect(converter.farenheit("farenheit", 50)).toBe(50);
+});
+
+test("from 50 farenheit to 10 celcius", () => {
+    expect(converter.farenheit("celcius", 50)).toBe(10);
 });
